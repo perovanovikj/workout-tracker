afa0ede72710971a168842e2e9adf1c841bbd53d
Rade Perovanovic <radeperovanovic22@gmail.com>
2026-02-10 18:35:19 +0100

    changes to backend

--- backend/src/main/java/com/workout/tracker/WorkoutTrackerApplication.java
+++ backend/src/main/java/com/workout/tracker/WorkoutTrackerApplication.java
@@ -2,7 +2,6 @@ package com.workout.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// testtesttest
 @SpringBootApplication
 public class WorkoutTrackerApplication {
     public static void main(String[] args) {
